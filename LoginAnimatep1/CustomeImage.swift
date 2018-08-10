4e7777cdac68b271f68869b607cbe7b1e7734dfa
mohsen khodadadzadeh <mohsenkhodadadzadeh@mohsens-macbook-pro.local>
2018-08-10 15:59:39 +0430

    First Push

---
+++ LoginAnimatep1/CustomeImage.swift
@@ -0,0 +1,43 @@
+//
+//  CustomeImage.swift
+//  LoginAnimatep1
+//
+//  Created by mohsen khodadadzadeh on 8/10/18.
+//  Copyright © 2018 mohsen khodadadzadeh. All rights reserved.
+//
+
+import UIKit
+
+class customeImage : UIImageView{
+   
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+            clipsToBounds = true
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+
+}
--- LoginAnimatep1/ViewController.swift
+++ LoginAnimatep1/ViewController.swift
@@ -10,11 +10,86 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButt: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        headerLabel.center.x = self.view.bounds.width / 2
+        username.center.x = self.view.bounds.width / 2
+        password.center.x = self.view.bounds.width / 2
+        loginButt.center.x = self.view.bounds.width / 2
+        
+        loginButt.layer.cornerRadius = 8.0
+        loginButt.layer.masksToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        // header label animate
+        UIView.animate(withDuration: 0.5) {
+            
+            self.headerLabel.center.x += self.view.bounds.width
+        }
+        
+        // username text field animate
+        UIView.animate(withDuration: 0.5, delay: 0.7, options: [],
+                       animations: {
+                        self.username.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
+        
+        
+        // password text field animate
+        UIView.animate(withDuration: 0.5, delay: 0.9, options: [],
+                       animations: {
+                        self.password.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
+        
+        //password text filed with repeat animate
+        /*
+        UIView.animate(withDuration: 0.5, delay: 0.4,
+                       options: .repeat,
+                       animations: {
+                        self.password.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
+        */
+        // password test field with couple animate
+        /*
+        UIView.animate(withDuration: 0.5, delay: 0.4,
+                       options: [.repeat, .autoreverse],
+                       animations: {
+                        self.password.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
+        */
+        
+        
+        
+        // password text field animate with curveEaseOut
+        /*
+        UIView.animate(withDuration: 0.5, delay: 0.4,
+                       
+                       options: [.repeat, .autoreverse, .curveEaseOut],
+                       animations: {
+                        self.password.center.x += self.view.bounds.width
+        },
+                       completion: nil
+        )
+        */
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        headerLabel.center.x  -= view.bounds.width
+        username.center.x -= view.bounds.width
+        password.center.x -= view.bounds.width
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
